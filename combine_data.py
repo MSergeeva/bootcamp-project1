68eab87fe6f6b796aa9c85cd0a1fc6d01a3d91ea
Marina Sergeeva <Marina.Sergeeva@capitalone.com>
2016-09-23 11:38:02 -0400

    combined data

--- combine_data.py
+++ combine_data.py
@@ -5,6 +5,28 @@ import datetime
 import pandas as pd
 import numpy as np
 
+def parse_genres(s):
+    if isinstance(s, list):
+        return s
+    genres = s.replace("[", "")
+    genres = genres.replace("]", "")
+    return genres.split(", ")
+
+def add_genre_dummy(lst, genre):
+    return 1 if genre in lst else 0
+
+def get_all_genres(df):
+    all_genres = set()
+    for genres in df["genres_list"]:
+        all_genres |= set(genres)
+    return list(all_genres)
+
+def add_columns_for_genres(all_genres, df):
+    for genre in all_genres:
+        df[genre] = 0
+    for genre in all_genres:
+        df[genre] = df["genres_list"].apply(lambda lst: add_genre_dummy(lst, genre))
+
 def main():
     cur_dir = os.path.dirname(os.path.realpath(__file__))
     data_dir = os.path.abspath(os.path.join(cur_dir, "data"))
@@ -26,8 +48,18 @@ def main():
     combined_df["year_release"] = combined_df["release_date_wide"].dt.year
     combined_df["log_domestic_gross"] = combined_df["domestic_gross"].apply(np.log)
     combined_df["log_opening_weekend_take"] = combined_df["opening_weekend_take"].apply(np.log)
+
+    print combined_df["genre"].head(3)
+    print type(combined_df["genre"][0])
+    combined_df["genres_list"] = combined_df["genre"].apply(parse_genres)
+    combined_df["number_of_genres"] = combined_df["genres_list"].apply(lambda x: len(x))
+    all_genres = get_all_genres(combined_df)
+
+    add_columns_for_genres(all_genres, combined_df)
+
     print combined_df.head(3)
     combined_df.to_csv("data/combined_movie_data.csv", encoding="utf-8")
+
 
 
 if __name__ == "__main__":
